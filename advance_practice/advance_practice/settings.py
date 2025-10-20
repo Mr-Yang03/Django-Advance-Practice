ba72cd897a823ce8678f4bc24cb45d4c4d712a56
Mr-Yang03 <duongphucthang2003@gmail.com>
2025-10-20 09:33:35 +0700

    set up to connect with Postgres DB

--- advance_practice/advance_practice/settings.py
+++ advance_practice/advance_practice/settings.py
@@ -72,10 +72,21 @@ WSGI_APPLICATION = 'advance_practice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_database',
+        'USER': 'admin',
+        'PASSWORD': 'thang123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
